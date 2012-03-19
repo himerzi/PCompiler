36a748b474ced857f96bb565b2bbd42fd8ccd298
Michael Detmold <michael.detmold@thatcrowdthing.com>
2012-03-19 20:44:22 +0000

    reverted fieldaccess

--- src/ast/expressions/FieldAccess.java
+++ src/ast/expressions/FieldAccess.java
@@ -4,8 +4,8 @@ package ast.expressions;
 import visitor.Visitor;
 
 public class FieldAccess extends ExprNode {
-	public FieldAccess(Id l, FieldAccess r) { left = l ; right = r; }
+	public FieldAccess(ExprNode l, FieldAccess r) { left = l ; right = r; }
 	//for whenever we have id.id, i.e. we only have one level of nesting
-	public FieldAccess(Id l,  Id r) { left = l ; right = r; }
+	public FieldAccess(ExprNode l,  ExprNode r) { left = l ; right = r; }
 	public Object accept(Visitor v) { return v.visit(this); }
 }
